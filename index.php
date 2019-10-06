4956df6e951f0075cba44a91fdccd57b06c8ef10
Calheiro <33173643+CesarCalheiro@users.noreply.github.com>
2019-10-06 10:35:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,25 +5,19 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
+ <head>
+  <meta charset="UTF-8">
+    <title></title>
+ </head>
     <body>
-        <?php
-        if (file_exists('vendor/autoload.php')) {
-    require 'vendor/autoload.php';
-}
-      include_once './application/libs/Application.php';
+         <?php if (file_exists('vendor/autoload.php')) {
+                 require 'vendor/autoload.php';
+               }
+               include_once './application/libs/Application.php';
     
-     // include_once 'application/controller/Home.php';
-     
-     // include_once './application/libs/Controller.php';
-$app = new Application();
-
-
-
-   
-?>
+                                      // include_once 'application/controller/Home.php';
+                                 // include_once './application/libs/Controller.php';
+               $app = new Application();
+         ?>
     </body>
 </html>
