f20f6567904c0e3feb189e9fd58eb96c93414fb8
Calheiro <33173643+CesarCalheiro@users.noreply.github.com>
2019-10-06 10:41:28 +0200

    Update Home.php

--- application/controller/Home.php
+++ application/controller/Home.php
@@ -1,60 +1,43 @@
 <?php
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  * Description of Home
  *
  * @author jonny
  */
+ include_once './application/libs/Controller.php';
+    class Home extends Controller{
+        private $songs;
+        private $query;
 
-include_once './application/libs/Controller.php';
+        public function index(){
+         require "application/views/wrapper.php";
+        }
+        public function getQuery(){ 
+            $this->songs = $this->loadModel('ME');
+            $this->query = $this->songs->query("select * from song");
+            return $this->query;
+        }
 
-class Home extends Controller {
+        public function loadForm(){
+         require "application/views/form.php";
+        }
 
-    private $songs;
-    private $query;
 
-    //put your code here
-    public function index() {
-  require "application/views/wrapper.php";
+        public function insert(){
+           $artist  =  $_POST['artist'];
+           $trank =  $_POST['trank'];
+           $link =  $_POST['link'];
+            if(isset($artist,$trank,$link)){
+              echo "Value inserted"."<br>" .$artist."<br>".$link."<br>".$trank;
+                $this->songs = $this->loadModel('ME');
+                $test =   $this->songs->query("INSERT INTO song (`artist`, `track`, `link`) VALUES('$artist','$trank','$link')");
+            }else {
+             echo 'error';
+            }
+        }
     }
-
-    public function getQuery() {
-         
-        $this->songs = $this->loadModel('ME');
-        $this->query = $this->songs->query("select * from song");
-        
-       
-        return $this->query;
-        
-      
-    }
-    
-    public function loadForm(){require "application/views/form.php"; }
-    
-    
-    public function insert()
-    {
-   $artist  =  $_POST['artist'];
-   $trank =  $_POST['trank'];
-    $link =  $_POST['link'];
-   if(isset($artist,$trank,$link)){
-       
-      echo "Value inserted"."<br>" .$artist."<br>".$link."<br>".$trank;
- 
-
-      
-    $this->songs = $this->loadModel('ME');
-    $test =   $this->songs->query("INSERT INTO song (`artist`, `track`, `link`) VALUES('$artist','$trank','$link')");
-   print_r($test);} else {
-    echo 'error';    
-}
-   
-    }
-   
-}
